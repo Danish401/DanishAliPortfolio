d0cf8882dcd6d31d08e6396124f9c3c64e0b3da7
Danish401 <alid13381@gmail.com>
2024-09-28 20:05:22 +0530

    first commit

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
 
 export default async function connectToDB() {
   try {
-    // if (mongoose.connections[0].readyState) return;
-    await mongoose.connect(
-      "mongodb+srv://CV:CV4010@cluster0.qpstf.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    if (mongoose.connections[0].readyState) return; // Check if already connected
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("Database connected successfully");
   } catch (e) {
-    console.log(e);
+    console.log("Database connection error: ", e);
   }
 }
